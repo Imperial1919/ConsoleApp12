450ef37b550f4f10aa2b5446f50daf67ee4627f8
Bryce <ImperialEagle1998@gmail.com>
2020-08-28 11:41:40 -0500

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+
+
+        public static void JadenCase(string phrase)
+        {
+          
+                Console.WriteLine(System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase));
+            
+        }
+        static void Main(string[] args)
+        {
+            JadenCase("hi there. my name is bryce.");
+        }
+    }
+}
